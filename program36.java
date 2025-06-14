5d42cf26a6c5edb45c5668ceb7c65bf96e3ce589
Bhakti Sojitra <sojitrabhakti03@gmail.com>
2025-06-15 00:37:32 +0530

    Update program36.java

--- program36.java
+++ program36.java
@@ -15,42 +15,6 @@ public class program36{
 
         // Step 2: Find intersection using retainAll
         setA.retainAll(setB);
-
-        // Step 3: Print result
         System.out.println(setA);  // Output in any order
-
-        /*Arrays.sort(a);
-        Arrays.sort(b);
-
-        List<Integer> temp1 = new ArrayList<>();
-        List<Integer> temp2 = new ArrayList<>();
-
-        // Start from the second element for 1st array
-        int index1 = 1;   
-        for(int i = 1; i < a.length; i++){
-            if (a[i] != a[i - 1]) 
-                a[index1++] = a[i];
-        }
-        for(int i = 0; i < index1; i++) 
-            temp1.add(a[i]);
-
-        // Start from the second element for 2nd array
-        int index2 = 1;   
-        for(int i = 1; i < b.length; i++){
-            if (b[i] != b[i - 1])
-                b[index2++] = b[i];
-        }
-        for(int i = 0; i < index2; i++)
-            temp2.add(b[i]);
-
-        System.out.println("temp1:- " + temp1);
-        System.out.println("temp2:- " + temp2);
-
-        for(int i = 0; i < temp1.size(); i++){
-            for(int j = 0; j < temp2.size(); j++){
-                if(temp1.get(i) == temp2.get(j))
-                    System.out.print(temp1.get(i) + " ");
-            }
-        }*/
     }
 }
